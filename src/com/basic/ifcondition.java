b3d721d2036b479bccf434f801ea2408291f5b61
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-05-29 18:29:16 +0530

    if condition

---
+++ src/com/basic/ifcondition.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class ifcondition {
+	public static void main(String args[]) {
+		int count=50;
+		if(count<65)
+			System.out.println("True");
+		else if(count>65)
+			System.out.println("False");
+		else
+		System.out.println("Default");
+	}
+
+}
