eef9d9fa2a97e05e63d6b5157968de9fb8568574
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-05-29 18:19:06 +0530

    operators

---
+++ src/com/basic/operators.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class operators {
+	public static void main(String args[]) {
+		int count=50;
+		count++;
+	count--;
+	int negcount=-20;
+	boolean isTrue=false;
+	System.out.println(!isTrue);
+	System.out.println(count>60);
+	System.out.println(count<100);
+	System.out.println(count>=45);
+	System.out.println(count<=35);
+	System.out.println(count==59);
+	System.out.println(count!=10);
+	System.out.println(5&5);
+	System.out.println(6|3);
+	System.out.println(6^3);
+	System.out.println(count==10 & isTrue);
+	System.out.println(count==10 | isTrue);
+	}
+
+}
+ 
