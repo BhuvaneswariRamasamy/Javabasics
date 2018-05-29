20d12146c9ab094432e28be48243e35e1e0d77a7
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-05-29 18:34:58 +0530

    switchcase

---
+++ src/com/basic/switchcase.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class switchcase {
+	public static void main(String args[]) {
+		int a=10;
+		switch(a) {
+		case 1:
+			System.out.println("Hello");
+			break;
+		case 10:
+			System.out.println("hello");
+			break;
+		default:
+		System.out.println("default");
+		break;
+		}
+		
+	}
+
+}
