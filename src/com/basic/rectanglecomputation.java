2371d4df35e36161c6f7796494a6e403ba3f3f52
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-05-30 11:27:02 +0530

    rectanglecomputation

---
+++ src/com/basic/rectanglecomputation.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class rectanglecomputation {
+	public static void main(String args[]) {
+		double area,perimeter,length=12;
+		double width=34;
+		area=length*width;
+		perimeter=2*(length+width);
+		System.out.println(area);
+		System.out.println(perimeter);
+	}
+
+}
