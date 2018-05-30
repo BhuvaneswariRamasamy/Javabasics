a8eaea019c5476a335c7d860927329c2cea13a4b
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-05-30 19:06:04 +0530

    object with primitive passing

--- src/com/basic/Car.java
+++ src/com/basic/Car.java
@@ -4,15 +4,46 @@ public class Car {
 
 	String model;
 	double speed;
-	Car()
+	 public Car()
 	{
 		
 	}
+	 
+	 
+	 
+	public String getModel() {
+		return model;
+	}
+
+
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+
+
+	public double getSpeed() {
+		return speed;
+	}
+
+
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+
+
 	public boolean start() {
 		return true;
 	}
 	public void accelerate() {
 		
+	}
+	public void change(int i)
+	{
+		i=i+10;
 	}
 	public static void main(String args[]) {
 		Car car1=new Car();
@@ -21,7 +52,10 @@ public class Car {
 		car2.speed=30.0;
 		System.out.println(car1.speed);
 		System.out.println(car2.speed);
-	}
+	int num=100;
+	car1.change(num);
+	System.out.println(num);
+  }
 	}
 
 
