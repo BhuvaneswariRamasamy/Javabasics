909735970056285cf74bc1dd2e3daeca12d4cde0
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-05-31 18:24:35 +0530

    It defines the DataTypeExample

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class DataTypeExample {
+	
+  public static void main(String args[]) {
+	  
+	  String firstName="Bhuvana";
+	  String secondName="Ramasamy";
+	  System.out.println("Bhuvana "+"Ramasamy");
+  }
+}
