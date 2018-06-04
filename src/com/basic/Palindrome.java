a96ee04715c30b6c05910241ad3cc06f06c4bef2
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-04 14:09:19 +0530

    To reverse a number

---
+++ src/com/basic/Palindrome.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public  class Palindrome {
+	 static int  N;int result=0;
+	public  int reverse(int N) {
+		while(N>0) {
+			int remainder=N%10;
+			N=N/10;
+		     result=result*10+remainder;
+		}
+		return result;
+	}
+public static void main(String args[]) {
+	Palindrome palindrome =new Palindrome();
+	palindrome.reverse(7631);
+    System.out.println(palindrome.reverse(N));
+
+}
+}
+
