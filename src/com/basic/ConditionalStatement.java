03c621a25cd2df7c0c61e7d15bf1f9af372a3659
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-05 14:06:02 +0530

    Examples for conditional statemaent programs

---
+++ src/com/basic/ConditionalStatement.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class ConditionalStatement { 
+	 static int age=65;
+	
+public static void main(String args[]) {	
+ if ( age >= 65 )
+System.out.println( "Age is greater than or equal to 65" );
+else
+System.out.println( "Age is less than 65");
+	}
+ 
+}
