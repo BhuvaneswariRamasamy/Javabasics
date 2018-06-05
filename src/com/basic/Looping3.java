9396309e5e1e32084626ca1d93fa632020b3bfde
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-05 15:01:34 +0530

    Examples fopr looping statements

---
+++ src/com/basic/Looping3.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class Looping3 {
+	public static void main(String args[]) {
+		int y=12,total=0;
+	while (y> 0 )
+	{
+		total +=y;
+		++y;
+	}
+	System.out.println( y );
+	++y;
+
+}
+}
