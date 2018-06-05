8ab262d01e6b9abdad790d07c109b9cca49ec8b4
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-05 14:54:00 +0530

    Example program for looping

---
+++ src/com/basic/Looping2.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class Looping2 {
+	public static void main(String args[]) {
+	int x=8,total=0;
+ while( x <= 100 )
+ {
+total += x;
+++x;
+}
+System.out.println(total);
+}
+}
