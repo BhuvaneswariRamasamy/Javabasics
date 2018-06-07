080a7e9e3015d793f3effb8fc756116849182bd3
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-07 14:51:35 +0530

    To print Fibonacci series

--- src/com/basic/FibonacciSeriess.java
+++ src/com/basic/FibonacciSeriess.java
@@ -1,10 +1,10 @@
 package com.basic;
 
-public class FibonacciSeries {
+public class FibonacciSeriess {
 public static void main(String args[]) {
 	int n1=0,n2=1,n3,count=10,i;
 
-	for(i=2;i<=10;i++) {
+	for(i=2;i<=count;i++) {
 		n3=n1+n2;
     System.out.print( n1+" " );   
 		n1=n2;
