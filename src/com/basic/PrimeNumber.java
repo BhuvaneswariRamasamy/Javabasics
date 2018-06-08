5b847c0da1fde44987890e727a961bfe6b269bcc
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-08 12:23:01 +0530

    To print PrimeNumber in Java

---
+++ src/com/basic/PrimeNumber.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class PrimeNumber {
+public static void main(String args[]) {
+	int n=10,i;
+for(i=1;i<=n;i++) {
+		if(n%i==0) {
+//			count++;
+			System.out.println("primenumber");}
+			else {
+				System.out.println("Not a prime number");	
+			}
+		}
+	}
+}
+
