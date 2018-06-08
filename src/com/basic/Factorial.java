c529f9edba81424bef09a1a5f02b3a37c4527469
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-08 12:23:47 +0530

    To print Factoria lusing Java

---
+++ src/com/basic/Factorial.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class Factorial {
+public static void main(String args[]) {
+	int i,n=5;
+	 int fact=1;
+	for(i=1;i<=n;i++) {
+		fact=fact*i;
+	}
+	System.out.println(" "+fact);
+}
+}
