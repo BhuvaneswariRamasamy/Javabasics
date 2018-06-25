68dfae1124ed802400dbacf150692fcb54648ad9
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-25 18:45:31 +0530

    Program to print Number Pattern

---
+++ src/com/basic/NumberPattern.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class NumberPattern {
+public static void main(String args[]) {
+	for(int i=1;i<=10;i++) {
+		for(int j=1;j<=i+0;j++) {
+			System.out.print("*");
+		}
+		System.out.println(" ");
+	}
+}
+}
