0547af0647163a0a6ceebbae58bc716567487ed0
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-25 18:52:46 +0530

    Program to print Numbers from 1-100 and 100-1

---
+++ src/com/basic/Numbers.java
@@ -0,0 +1,19 @@
+package com.basic;
+
+public class Numbers {
+public static void main(String args[]) {
+	int n=1;
+	System.out.println("Numbers from 100-1:");
+	for(int i=100;i>=n;i=i-2) {
+		
+		System.out.println(i);
+	}
+	System.out.println(" ");
+	int n1=100;
+	
+		System.out.println("Numbers from 1-100:");
+		for(int i=1;i<n1;i++) {
+		System.out.println(i);
+	}
+}
+}
